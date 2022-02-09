204a7ed5d013f7b2c980711c0a5fb3aa278ddbe9
Nourcel <Kal21016@byui.edu>
2022-02-09 04:40:44 +0200

    done

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -5,3 +5,14 @@ function togglemenu(){
 
 const x = document.getElementById("humbugerbtn")
 x.onclick = togglemenu;
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+ 
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
